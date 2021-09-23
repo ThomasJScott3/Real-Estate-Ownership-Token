461ef6f5ead276bab069f9576caa35f029663b8f
YonathanTE <yonathanteshetu@gmail.com>
2021-09-23 19:48:13 -0400

    Removing some material

--- PrototypeNFT.sol
+++ PrototypeNFT.sol
@@ -18,32 +18,28 @@ contract Prototype is ERC721Full {
 
     using Counters for Counters.Counter;
     Counters.Counter token_ids;
-    
-    // We need to get the deed/Title value in here for Proof of Ownership
+    // Features of the house 
     struct House {
-        string name;
+        string name; // Basically the address 'e.g. 1600 Washington Avenue'
         uint yearBuilt;
         uint appraisal_value;
-        uint initial_value;
-        string homeBuilder;
+        uint initial_value; // What we say the starting price will be
+        string homeBuilder; 
         uint bedrooms;
         uint bathrooms;
         uint square_feet;
-        uint breakIns;
     }
+
   // project_overlord can be thought of as a broker with a specific collection of houses/NFTs that we'd add based on this code
   mapping(uint => House) public project_overlord;
-  mapping(uint => House) public houses; 
 
-  // event HouseforSale is already posted above so I'm not writing it out again.
+  // Declare an event
   event HouseforSale(uint token_id, uint appraisal_value, string report_uri, uint bedrooms, uint bathrooms, uint square_feet);
-
-  event breakIn(uint token_id, string report_uri);
   
   // This will later be used in an emit to display what'd we'd like to show on chain 
   // For example, say we only wanted to add the information from above on chain when we're selling the house and putting it up for auction:
   function registerHouse(address owner, string memory name, uint yearBuilt, uint appraisal_value, string memory homeBuilder, uint initial_value, string memory token_uri, uint bedrooms, uint bathrooms, uint square_feet, uint breakIns) public returns(uint) {
-        token_ids.increment(); // adding another token_id on the chain since we're adding a house to the market/auction (If there's a better way of explaining this, feel free to mention this)
+        token_ids.increment(); // adding another token_id on the chain since we're adding a house to the market/auction 
         uint token_id = token_ids.current();
 
         _mint(owner, token_id); // This is from ERC721Full.sol. However, take note that it's from ERC721Enumerable.sol, a parent contract of ERC721Full.sol
